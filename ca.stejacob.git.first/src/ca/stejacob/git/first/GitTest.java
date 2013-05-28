3341f405c680a56ccace51f487cb8655cff28580
Stephen Jacob <stephen.jacob@norconex.com>
2013-05-28 16:47:59 -0400

    Initial commit

---
+++ ca.stejacob.git.first/src/ca/stejacob/git/first/GitTest.java
@@ -0,0 +1,13 @@
+package ca.stejacob.git.first;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+		System.out.println("Testing the staging area");
+	}
+
+}
