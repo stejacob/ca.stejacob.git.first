8d89e87c3e6e9f014740021c916ca1c2c1febe4f
Stephen Jacob <stephen.jacob@norconex.com>
2013-05-31 15:20:22 -0400

    Initial Commit.

--- AppTestGit/src/main/java/ca/stejacob/git/first/GitTest.java
+++ AppTestGit/src/main/java/ca/stejacob/git/first/GitTest.java
@@ -1,11 +1,19 @@
+/**
+ * 
+ */
 package ca.stejacob.git.first;
 
+/**
+ * @author lgreville
+ *
+ */
 public class GitTest {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Git is cool");
+		System.out.println("My First Maven/GIT project. sweet.");
 	}
+
 }
